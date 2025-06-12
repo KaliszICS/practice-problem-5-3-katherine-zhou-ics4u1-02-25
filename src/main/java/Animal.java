6caf3f7dc7d297481b6a3201090164b0dad25a28
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-06-12 02:23:48 +0000

    Update Animal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,40 +1,36 @@
 abstract class Animal{
 
-    protected String name;
-    protected int age;
-    protected String type;
-    protected int position;
+    protected String name, type;
+    protected int age, position;
 
     public Animal(String name, int age, String type, int position){
-        this.name = name;
-        this.age = age;
-        this.type = type;
-        this.position = position;
+        this.name=name;
+        this.type=type;
+        this.age=age;
+        this.position=position;
     }
 
+    abstract String animalSound();
 
+    abstract void move();
 
-    public String Sleep(){
-        return "Zzz" ;
+    public String sleep(){
+        return "Zzz";
     }
 
     public String getName(){
         return this.name;
     }
 
-    public int getAge(){
-        return this.age;
-    }
-
     public String getType(){
         return this.type;
+    }
+
+    public int getAge(){
+        return this.age;
     }
 
     public int getPosition(){
         return this.position;
     }
-
-    abstract String animalSound();
-    abstract void move();
-
 }
